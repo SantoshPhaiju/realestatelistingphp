7c2d5444ec441d3184308a7d14cdf3763759f443
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-17 21:07:48 +0545

    Project setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real State Listing Site</title>
+</head>
+<body>
+    <h1>
+        Welcome to the Real State Listing Site!
+    </h1>
+</body>
+</html>
